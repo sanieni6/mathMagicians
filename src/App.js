fe2d99d14abb8291c679734981974c403b5bab76
sanieni6 <sanieni6@gmail.com>
2023-03-23 13:25:18 -0500

    update the components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Calc from './components/Calculator';
+import Quote from './components/quote';
 
 function App() {
   return (
-    <Calc />
+    <div className="main-container">
+      <Calc />
+      <Quote />
+
+    </div>
   );
 }
 
