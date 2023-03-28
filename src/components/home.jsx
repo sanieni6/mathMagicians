99a6462b89011d04204f4594aec7415194efd9d1
sanieni6 <sanieni6@gmail.com>
2023-03-28 16:19:11 -0500

    Create the home page

---
+++ src/components/home.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../scss/home.scss';
+
+const Home = () => (
+  <div className="home__container">
+    <h2 className="home__container-title">Welcome to our page!</h2>
+    <p className="home__container-ipsum">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elit augue, feugiat
+      sed accumsan ut, maximus in augue. Cras pellentesque odio a commodo pretium. Aenean
+      tincidunt elit nec orci lacinia, at porta lectus euismod. Ut dignissim nulla ante,
+      eget tempus ante scelerisque vitae. Cras in mi cursus, dictum magna nec, feugiat est.
+      Mauris at ante eget velit convallis cursus id eget mi. Curabitur sit amet faucibus
+      magna. Phasellus nec porta leo. Aenean pulvinar convallis interdum. Praesent nec metus
+      viverra, aliquam nulla eu, tempor nulla. Pellentesque tempor, mauris nec sollicitudin
+      iaculis, mi purus tincidunt augue, vel mattis lorem est at purus. Integer feugiat augue
+      in leo ornare efficitur. Aliquam ullamcorper ex a neque pretium convallis. Etiam luctus
+      dui dui, eget egestas leo rutrum ut. Aenean non dignissim sapien, quis laoreet tellus.
+    </p>
+    <p className="home__container-ipsum">
+      Nulla commodo rhoncus ornare. Vivamus ac tristique felis, vel lacinia risus. Maecenas et
+      orci vel augue faucibus semper sit amet in quam. Aliquam gravida lectus non ligula
+      vehicula, sit amet efficitur odio elementum. Mauris at ante commodo, aliquam enim
+      hendrerit, sollicitudin turpis. Vivamus consectetur ipsum quis libero ultricies, ut
+      molestie magna lobortis. Pellentesque commodo turpis nibh.
+    </p>
+  </div>
+
+);
+
+export default Home;
